76dbec9cd63d797b82b78701043c9d10fef997f7
harshdhuppad05 <87632627+harshdhuppad05@users.noreply.github.com>
2025-11-24 07:15:57 +0530

    Create 24 November 2025, 1018.cpp

---
+++ Leetcode Daily/November/24 November 2025, 1018.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<bool> prefixesDivBy5(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> ans(n, false);
+        int val = 0;
+        for(int i=0;i<n;i++){
+            val = (val*2 + nums[i])%5;
+            if(val%5 ==0)ans[i] = true;
+        }
+        return ans;
+    }
+};
