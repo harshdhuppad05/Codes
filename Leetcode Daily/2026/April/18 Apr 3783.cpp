0c0b3c60c032d6f2d25f9a763c73f37021bea590
harshdhuppad05 <87632627+harshdhuppad05@users.noreply.github.com>
2026-04-18 21:56:22 +0530

    Create 18 Apr 3783.cpp

---
+++ Leetcode Daily/2026/April/18 Apr 3783.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int mirrorDistance(int n) {
+        long long rev=0;
+    for(int x=n; x; x/=10){
+        rev=10*rev+x%10;
+    }
+    return abs(rev-n);
+    }
+};
