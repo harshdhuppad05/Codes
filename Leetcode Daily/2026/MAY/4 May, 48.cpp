31952b722794c2ee860a7478ffd9b26c34744e9e
harshdhuppad05 <87632627+harshdhuppad05@users.noreply.github.com>
2026-05-04 11:34:21 +0530

    Create 4 May, 48.cpp

---
+++ Leetcode Daily/2026/MAY/4 May, 48.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& mat) {
+        int n = mat.size();
+        for(int i=0;i<n/2;i++){
+            for(int j=0;j<n;j++){
+                swap(mat[n-1-i][j], mat[i][j]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                swap(mat[i][j], mat[j][i]);
+            }
+        }
+    }
+};
