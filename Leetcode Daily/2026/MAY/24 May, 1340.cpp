9b11bb8ff9ff5bdde9c774129bc32c7ec5b4cee5
harshdhuppad05 <87632627+harshdhuppad05@users.noreply.github.com>
2026-05-24 09:53:19 +0530

    Create 24 May, 1340.cpp

---
+++ Leetcode Daily/2026/MAY/24 May, 1340.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    int dfs(int i, vector<int>& arr, int d, vector<int>& dp) {
+        if (dp[i] != -1) return dp[i];
+
+        int n = arr.size();
+        int ans = 1;
+
+        // go left
+        for (int j = i - 1; j >= max(0, i - d); j--) {
+
+            // cannot cross higher/equal element
+            if (arr[j] >= arr[i]) break;
+
+            ans = max(ans, 1 + dfs(j, arr, d, dp));
+        }
+
+        // go right
+        for (int j = i + 1; j <= min(n - 1, i + d); j++) {
+
+            if (arr[j] >= arr[i]) break;
+
+            ans = max(ans, 1 + dfs(j, arr, d, dp));
+        }
+
+        return dp[i] = ans;
+    }
+
+    int maxJumps(vector<int>& arr, int d) {
+
+        int n = arr.size();
+
+        vector<int> dp(n, -1);
+
+        int ans = 1;
+
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, dfs(i, arr, d, dp));
+        }
+
+        return ans;
+    }
+};
