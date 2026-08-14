facbdc367e58ba20243d7e9ef1125ae100ff79f6
harshdhuppad05 <87632627+harshdhuppad05@users.noreply.github.com>
2026-08-14 09:34:54 +0530

    Create 14 August, 3090.cpp

---
+++ Leetcode Daily/2026/August/14 August, 3090.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        int n=s.size();
+        int i=0,j=0;
+        int ans=0;
+        unordered_map<char,int>mp;
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+            while(mp[s[i]]>2){
+                mp[s[j++]]--;
+            }
+            ans=max(ans, i-j+1);
+        }
+        return ans;
+    }
+};
